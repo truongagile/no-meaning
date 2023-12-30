2e7072e1cce695137aa9e913bc3a27f1460b7415
truongagile <145410134+truongagile@users.noreply.github.com>
2023-12-30 09:56:03 +0700

    Revert "edit home page page v1.0.0"

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,19 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Home Page</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Read more
+          Learn React
         </a>
       </header>
     </div>
