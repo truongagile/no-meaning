15037679d9040e369475d06561b943eca30f08c6
Truong Nguyen <truong.agile@gmail.com>
2023-12-30 09:58:36 +0700

    fix after revert

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Read more
+          Read more after revert
         </a>
       </header>
     </div>
